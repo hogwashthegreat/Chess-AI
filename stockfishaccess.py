f08d4ad0c70dfac9d8bd1ce9adebef8bac7621b9
hogwashthegreat <89415692+hogwashthegreat@users.noreply.github.com>
2023-04-25 13:26:32 -0700

    Stockfish Access Utility
    requires stockfish folder to be installed

---
+++ stockfishaccess.py
@@ -0,0 +1,27 @@
+import chess
+import chess.engine
+import random
+
+
+def stockfish_evaluation(board, depth_=10):
+        engine = chess.engine.SimpleEngine.popen_uci("stockfish\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe")
+        result = engine.analyse(board, chess.engine.Limit(depth=depth_))
+        if result["score"].is_mate():
+            print("mate")
+            print(result["score"].white().mate())
+            return result["score"].white().mate()
+        print(board.fen)
+        print(result["score"].white().score())
+        return result["score"].white().score()/100
+
+def random_board(max_depth=200):
+        board = chess.Board()
+        depth = random.randrange(1, max_depth)
+
+        for i in range(depth):
+            all_moves = list(board.legal_moves)
+            random_move = random.choice(all_moves)
+            board.push(random_move)
+            if board.is_game_over():
+                break
+        return board
