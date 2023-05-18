be39e4983a955bb569c43c1656e651dc9b09e822
troymerr <124810698+troymerr@users.noreply.github.com>
2023-05-18 09:35:57 -0700

    Delete finalproject directory

--- finalproject/static/js/script.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  draggable: true,
-  dropOffBoard: 'snapback', // this is the default
-  position: 'start'
-}
-var board = Chessboard('board1', config)
-$('#startBtn').on('click', board.start)
-$('#clearBtn').on('click', board.clear)
